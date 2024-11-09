e6750a056403d8c874b34afaaf803a4f51d436ea
Michael Xie <mxie0114@gmail.com>
2024-11-09 08:40:56 -0500

    Installed ApolloClient

---
+++ clubconnect/server/server.js
@@ -0,0 +1,4 @@
+import { ApolloServer } from "@apollo/server";
+import mongoose from "mongoose";
+
+const string = import.meta.env.VITE_URI
--- clubconnect/src/main.jsx
+++ clubconnect/src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import {NextUIProvider} from '@nextui-org/react'
 import './index.css'
 import App from './App.jsx'
+import.meta.env.VITE_URI
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const server = new ApolloClient({
+  uri: import.meta.env.VITE_URI,
+  cache: new InMemoryCache(),
+})
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
